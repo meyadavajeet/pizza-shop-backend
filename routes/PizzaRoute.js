6e56ef9ed0f513c44e8bef5054218a9177e33385
Ajeet Yadav <meyadavajeet@gmail.com>
2022-06-08 08:52:26 +0530

    working on change localdb

--- routes/PizzaRoute.js
+++ routes/PizzaRoute.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const PizzaModel = require('../models/PizzaModel');
 
 /*
-@GET REQUEST
-GET ALL PIZZAS
+*@GET REQUEST
+*GET ALL PIZZAS
 */
-router.get("/get-all-pizzas",async (req, res) => {
-  try {
-    const pizzas = await PizzaModel.find({});
-    res.send(pizzas);
-  } catch (error) {
-    res.json({message: error + "Something went wrong."});
-  }
+router.get("/get-all-pizzas", async (req, res) => {
+	try {
+		let pizzas = await PizzaModel.find({});
+		res.send(pizzas);
+	} catch (err) {
+		res.json({ message: err + "Something went wrong." });
+	}
 
 });
 module.exports = router;
